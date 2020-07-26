9a9beb9200639c6a6cc9e6cecb3da323a2d65fa4
Gabriel Perez <gapc87@gmail.com>
2020-07-26 11:49:17 +0200

    Update Currency.php

--- app/Currency.php
+++ app/Currency.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
-    //
+    protected $primaryKey = 'iso';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'iso'
+    ];
 }
